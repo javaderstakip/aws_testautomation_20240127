915d9b649bbe9133b28373195a77c6e4fea6febf
javaderstakip <mehmetjavaderstakip@gmail.com>
2024-02-10 21:57:46 +0300

    i think it is successfully

--- src/test/java/stepDefinitions/DriverSource.java
+++ src/test/java/stepDefinitions/DriverSource.java
@@ -11,12 +11,19 @@ public enum DriverSource {
     ChromeOptions options = new ChromeOptions();
     private WebDriver driver;
 
+//    public WebDriver getDriver(){
+//        if (driver == null){
+//            driver = new ChromeDriver();
+//            //options.addArguments("--headless");// addArguments kullandık.
+//            //driver = new ChromeDriver(options);
+//            options.addArguments("headless");
+//            driver = new ChromeDriver(options);
+//        }
+//        return driver;
+//    }
     public WebDriver getDriver(){
         if (driver == null){
-            driver = new ChromeDriver();
-            //options.addArguments("--headless");// addArguments kullandık.
-            //driver = new ChromeDriver(options);
-            options.addArguments("headless");
+            options.addArguments("--headless"); // Headless argümanı eklendi
             driver = new ChromeDriver(options);
         }
         return driver;
